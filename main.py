3faa9565c60af59f8f6ec68b635b10fb03e3f3be
Josef Kolář <thejoeejoee@gmail.com>
2022-04-08 17:20:40 +0200

    Tweaked conf

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 #!/bin/env python3
+import logging
 from pprint import pprint
 
 import click
 import instagram_scraper
-import logging
+from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.DEBUG)
 
+load_dotenv()
+
 
 @click.command()
-@click.argument('username')
-@click.argument('password')
-@click.argument('tag', default='egmporto2022')
-def main(username, password, tag):
+@click.option('--ig-username')
+@click.option('--ig-password')
+@click.option('--hashtag', default='egmporto2022')
+def main(ig_username, ig_password, hashtag):
     insta_scraper = instagram_scraper.InstagramScraper(
-        login_user=username, login_pass=password, tag=True, username=tag
+        login_user=ig_username, login_pass=ig_password,
+        tag=True, username=hashtag
     )
     insta_scraper.authenticate_with_login()
 
-    for i in insta_scraper.query_hashtag_gen(tag):
+    for i in insta_scraper.query_hashtag_gen(hashtag):
         pprint(i)
         break
 
+
 if __name__ == '__main__':
-    main()
+    main(auto_envvar_prefix='_')
